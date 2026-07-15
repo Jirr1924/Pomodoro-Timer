371cdde717286b629bc4e1361a9ebd98bf95d171
Muhammad Aljir Romdhan <mefor1260@gmail.com>
2026-07-15 23:05:06 +0800

    "Update konfigurasi Firebase" atau "Fix Firebase API Key").

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,11 +1,20 @@
 import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import firebaseConfig from '../../firebase-applet-config.json';
+
+const firebaseConfig = {
+  projectId: "cozypomo-timer",
+  appId: "1:497085990693:web:6378bd51d2896bebf5fccc",
+  apiKey: "AIzaSyDuBf3hdN-cZg07Wz3JSEcxdH9gsYKXwgk",
+  authDomain: "cozypomo-timer.firebaseapp.com",
+  storageBucket: "cozypomo-timer.firebasestorage.app",
+  messagingSenderId: "497085990693",
+  measurementId: "G-6Y1P59RVEL"
+};
 
 // Initialize Firebase App
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 export const auth = getAuth(app);
-export const db = getFirestore(app, (firebaseConfig as any).firestoreDatabaseId);
+export const db = getFirestore(app, "(default)");
 export const googleProvider = new GoogleAuthProvider();
