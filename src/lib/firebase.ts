b49f430756ac17d8b712cfd3ef0edccd0f67c065
Muhammad Aljir Romdhan <mefor1260@gmail.com>
2026-07-15 23:14:52 +0800

    jangan di deploy dulu

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,20 +1,11 @@
 import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-
-const firebaseConfig = {
-  projectId: "cozypomo-timer",
-  appId: "1:497085990693:web:6378bd51d2896bebf5fccc",
-  apiKey: "AIzaSyDuBf3hdN-cZg07Wz3JSEcxdH9gsYKXwgk",
-  authDomain: "cozypomo-timer.firebaseapp.com",
-  storageBucket: "cozypomo-timer.firebasestorage.app",
-  messagingSenderId: "497085990693",
-  measurementId: "G-6Y1P59RVEL"
-};
+import firebaseConfig from '../../firebase-applet-config.json';
 
 // Initialize Firebase App
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 export const auth = getAuth(app);
-export const db = getFirestore(app, "(default)");
+export const db = getFirestore(app, (firebaseConfig as any).firestoreDatabaseId);
 export const googleProvider = new GoogleAuthProvider();
